b590a390e5105471165b1fc294d54298d06ca39c
anjalidisa <68415735+anjalidisa@users.noreply.github.com>
2020-12-11 17:40:27 +0530

    Program to draw a star using Turtle Library
    Program to draw a star using Turtle Library

---
+++ Draw_star.py
@@ -0,0 +1,11 @@
+import turtle
+screen = turtle.Screen()
+t = turtle.Turtle()
+for i in range(5):
+  t.pencolor("yellow")
+  t.fillcolor("yellow")
+  t.begin_fill()
+  t.fd(200)
+  t.rt(144)
+t.end_fill()
+t.ht()
